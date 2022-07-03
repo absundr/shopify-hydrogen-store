2cab497e056f0c7585ef65123b6a7fa149d32168
abhijith sunder <abhijithsunder01@gmail.com>
2022-07-03 17:21:33 +0530

    initial commit

---
+++ hydrogen.config.js
@@ -0,0 +1,18 @@
+import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
+
+export default defineConfig({
+  shopify: {
+    defaultCountryCode: 'US',
+    defaultLanguageCode: 'EN',
+    storeDomain: 'https://mystore-0c11.myshopify.com',
+    storefrontToken: 'd6a468262499f87c3a9689ee81f2edad',
+    storefrontApiVersion: '2022-07',
+  },
+  session: CookieSessionStorage('__session', {
+    path: '/',
+    httpOnly: true,
+    secure: import.meta.env.PROD,
+    sameSite: 'Strict',
+    maxAge: 60 * 60 * 24 * 30,
+  }),
+});
